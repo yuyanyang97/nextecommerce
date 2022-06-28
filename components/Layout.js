3b15a7ce14f12fc8737941efeaa9bc414ac9b0b0
yuyanyang97 <yuyanyang97@gmail.com>
2022-06-29 04:27:07 +0800

    add product

--- components/Layout.js
+++ components/Layout.js
@@ -33,7 +33,9 @@ export default function Layout({ children, title }) {
             </div>
           </nav>
         </header>
-        <main className="container m-auto mt-4 px-4">{children}</main>
+        <main className="container m-auto mt-4 px-4">
+          {children}
+        </main>
         <footer className="flex h-10 justify-center items-center shadow-inner">footer</footer>
       </div>
     </>
---
+++ ultils/data.js
@@ -0,0 +1,30 @@
+const data = {
+    products :[
+        {
+            name: 'Free Shirt',
+            slug: 'free-shirt',
+            category: 'Shirt',
+            image: '/images/shirt1.jpg',
+            price: 70,
+            brnad: 'Nike',
+            rating: 4.5,
+            numReviews: 8,
+            countinStock: 20,
+            description: 'A popular shirt'
+        },
+        {
+            name: 'Fit Shirt',
+            slug: 'fit-shirt',
+            category: 'Shirt',
+            image: '/images/shirt2.jpg',
+            price: 70,
+            brnad: 'Adidas',
+            rating: 3.2,
+            numReviews: 11,
+            countinStock: 2,
+            description: 'A  shirt'
+        },
+    ],
+};
+
+export default data;
